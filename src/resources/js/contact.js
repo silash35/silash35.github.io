2dc451c674f658225a9bbfb83940a8134350009c
Silas Henrique <silash35@gmail.com>
2020-11-15 15:54:54 -0300

    fix build error

--- src/resources/js/contact.js
+++ src/resources/js/contact.js
@@ -1,7 +1,11 @@
 import configHeader from "./modules/_configHeader";
-import {MDCTextField} from '@material/textfield/index';
+import {MDCTextField} from "@material/textfield/index";
 configHeader();
 
-const textField = [].map.call(document.querySelectorAll('.mdc-text-field'), function(el) {
+const textField = [].map.call(document.querySelectorAll(".textField"), function(el) {
+  return new MDCTextField(el);
+});
+
+const textArea = [].map.call(document.querySelectorAll(".textArea"), function(el) {
   return new MDCTextField(el);
 });
