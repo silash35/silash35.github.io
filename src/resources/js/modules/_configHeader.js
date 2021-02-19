eaeb39aeb4cdfbeef8834280ee9a20e33f35a6b6
Silas Henrique <silash35@gmail.com>
2021-02-19 16:01:46 -0300

    improve sideNav performance

--- src/resources/js/modules/_configHeader.js
+++ src/resources/js/modules/_configHeader.js
@@ -19,13 +19,13 @@ function setHeaderActive() {
 
 function globalizeSidenavFunctions() {
   globalize("openSidenav", () => {
-    navLinks.style.left = "0px";
+    navLinks.style.transform = "translateX(300px)";
     screenDarkener.style.visibility = "visible";
     screenDarkener.style.opacity = ".2";
   });
 
   globalize("closeSidenav", () => {
-    navLinks.style.left = "-300px";
+    navLinks.style.transform = "translateX(0)";
     screenDarkener.style.opacity = "0";
 
     // waits the animation ends
@@ -35,7 +35,20 @@ function globalizeSidenavFunctions() {
   });
 }
 
+function closeSidenavOnMedia() {
+  const mediaQuery = window.matchMedia("(min-width: 800px)");
+
+  const handleScreenChange = (e) => {
+    if (e.matches) {
+      closeSidenav();
+    }
+  };
+
+  mediaQuery.addEventListener("change", handleScreenChange);
+}
+
 export default () => {
   setHeaderActive();
   globalizeSidenavFunctions();
+  closeSidenavOnMedia();
 };
