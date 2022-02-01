1ca6f07e75ac95cfeb0c3c020132614ad0ec296c
Silas Henrique <silash35@gmail.com>
2022-02-01 17:47:08 -0300

    fix service worker location

--- src/resources/js/utils/installServiceWorker.js
+++ src/resources/js/utils/installServiceWorker.js
@@ -1,6 +1,6 @@
 export default () => {
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register(new URL("service-worker.js", import.meta.url), {
+    navigator.serviceWorker.register(new URL("/src/service-worker.js", import.meta.url), {
       type: "module",
     });
   }
