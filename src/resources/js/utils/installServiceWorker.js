72878737e736d1bc454aba66f618886a3bb28a5d
Silas Henrique <silash35@gmail.com>
2022-02-01 17:06:01 -0300

    add @parcel/service-worker

--- src/resources/js/utils/installServiceWorker.js
+++ src/resources/js/utils/installServiceWorker.js
@@ -1,6 +1,7 @@
 export default () => {
   if ("serviceWorker" in navigator) {
-    const sw = "/service-worker.js";
-    navigator.serviceWorker.register(sw);
+    navigator.serviceWorker.register(new URL("service-worker.js", import.meta.url), {
+      type: "module",
+    });
   }
 };
