47dbf926eeaf81d9f9f078126312fc9f005eb31c
Silas Henrique <silash35@gmail.com>
2022-02-01 16:26:54 -0300

    add compressors

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   globDirectory: "dist",
-  globPatterns: ["**/*.{html,js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map,webp}"],
+  globPatterns: [
+    "**/*.{html,js,css,png,svg,jpg,gif,json,gz,br,woff,woff2,eot,ico,webmanifest,map,webp}",
+  ],
   swDest: "dist/service-worker.js",
   clientsClaim: true,
   skipWaiting: true,
