a682f686c742d801c8b5cb5d1c03911627adfc76
Silas Henrique <silash35@gmail.com>
2022-02-01 18:48:18 -0300

    fixing SW

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   globDirectory: "dist/",
   globPatterns: [
-    "**/*.{html,js,css,png,svg,jpg,gif,json,gz,br,woff,woff2,eot,ico,webmanifest,map,webp}",
+    "**/*.{html,js,css,png,svg,gz,br,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map,webp}",
   ],
   swDest: "dist/service-worker.js",
+  clientsClaim: true,
+  skipWaiting: true,
 };
