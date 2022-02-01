0adde2b808f148de54c44f60acc8aa5436110b16
Silas Henrique <silash35@gmail.com>
2022-02-01 18:53:38 -0300

    fix SW register

--- src/resources/js/utils/installServiceWorker.js
+++ src/resources/js/utils/installServiceWorker.js
@@ -1,9 +1,7 @@
 export default () => {
   if ("serviceWorker" in navigator) {
     // Use the window load event to keep the page load performant
-    window.addEventListener("load", () => {
-      const sw = "/service-worker.js";
-      navigator.serviceWorker.register(sw);
-    });
+    const sw = "/service-worker.js";
+    navigator.serviceWorker.register(sw);
   }
 };
--- workbox-config.js
+++ workbox-config.js
@@ -4,6 +4,5 @@ module.exports = {
     "**/*.{html,js,css,png,svg,gz,br,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map,webp}",
   ],
   swDest: "dist/service-worker.js",
-  clientsClaim: true,
-  skipWaiting: true,
+  cleanupOutdatedCaches: true,
 };
