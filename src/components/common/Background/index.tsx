a902f989febeedf4e2dacbd8b69e72531e03eacc
Silas Henrique <silash35@gmail.com>
2022-03-24 16:16:03 -0300

    remove next image

--- src/components/common/Background/index.tsx
+++ src/components/common/Background/index.tsx
@@ -1,15 +1,11 @@
-import Image from "next/image";
-
 import styles from "./background.module.scss";
 
 export default function Background() {
   return (
-    <div className={styles.bgWrap}>
-      <Image
+    <div className={styles.background}>
+      <img
         alt="Black background with incandescent light bulbs"
         src="/images/power.webp"
-        layout="fill"
-        objectFit="cover"
         aria-hidden="true"
       />
     </div>
