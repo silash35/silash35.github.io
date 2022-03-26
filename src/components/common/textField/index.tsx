fdcb0c98fed49be051e0ac96ee4d00db62932492
Silas Henrique <silash35@gmail.com>
2022-03-26 16:37:03 -0300

    add textfield component

---
+++ src/components/common/textField/index.tsx
@@ -0,0 +1,24 @@
+import type { InputHTMLAttributes } from "react";
+
+import styles from "./textField.module.scss";
+
+interface Props {
+  text: string;
+  textareaProps?: InputHTMLAttributes<HTMLTextAreaElement>;
+  inputProps?: InputHTMLAttributes<HTMLInputElement>;
+}
+
+const TextField = ({ text, inputProps, textareaProps }: Props) => {
+  return (
+    <label className={styles.textfield}>
+      {textareaProps ? (
+        <textarea placeholder=" " {...textareaProps} />
+      ) : (
+        <input placeholder=" " {...inputProps} />
+      )}
+      <span>{text}</span>
+    </label>
+  );
+};
+
+export default TextField;
--- src/components/contact/card/form.tsx
+++ src/components/contact/card/form.tsx
@@ -1,23 +1,37 @@
+import TextField from "@/components/common/textField";
+
 import styles from "./form.module.scss";
 
 const Form = () => {
   return (
     <section className={styles.form}>
       <form action="https://formsubmit.co/silash35@gmail.com" method="POST">
-        <label>
-          Name
-          <input name="name" aria-label="Enter your name" required />
-        </label>
+        <TextField
+          text="Name"
+          inputProps={{
+            name: "name",
+            "aria-label": "Enter your name",
+            required: true,
+          }}
+        />
 
-        <label>
-          E-mail
-          <input type="email" name="email" aria-label="Enter your email address" required />
-        </label>
+        <TextField
+          text="Email"
+          inputProps={{
+            type: "email",
+            name: "email",
+            "aria-label": "Enter your email address",
+            required: true,
+          }}
+        />
 
-        <label>
-          Message
-          <textarea name="message" aria-label="Enter your message" />
-        </label>
+        <TextField
+          text="Message"
+          textareaProps={{
+            name: "message",
+            "aria-label": "Enter your message",
+          }}
+        />
 
         <button type="submit">
           <img src="/images/icons/black/send.svg" alt="paper plane icon" />
