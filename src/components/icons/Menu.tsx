c67e840c6d8c243b77f5e4a19b1c3a6796447f13
Silas Henrique <silash35@gmail.com>
2024-05-27 12:13:02 -0300

    move svg icons to tsx

---
+++ src/components/icons/Menu.tsx
@@ -0,0 +1,8 @@
+const MenuIcon = () => (
+  <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+    <title>Menu</title>
+    <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+  </svg>
+);
+
+export default MenuIcon;
---
+++ src/components/icons/Send.tsx
@@ -0,0 +1,8 @@
+const SendIcon = () => (
+  <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+    <title>Send</title>
+    <path d="M2,21L23,12L2,3V10L17,12L2,14V21Z" />
+  </svg>
+);
+
+export default SendIcon;
