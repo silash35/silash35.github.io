f4384ff3224675c2ff07d85d5d1fb2492f1b53ef
Silas Henrique <silash35@gmail.com>
2024-06-11 22:38:11 -0300

    simplify next config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,20 @@
 import BundleAnalyzer from "@next/bundle-analyzer";
-import * as path from "path";
-import { fileURLToPath } from "url";
-
 /**
  * @type {import('next').NextConfig}
  */
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+let nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  sassOptions: {
+    includePaths: ["./src/styles"],
+  },
+  env: {
+    SITE_URL: process.env.SITE_URL,
+  },
+};
 
 export default () => {
-  let nextConfig = {
-    reactStrictMode: true,
-    output: "export",
-    sassOptions: {
-      includePaths: [path.join(__dirname, "src/styles")],
-    },
-    env: {
-      SITE_URL: process.env.SITE_URL,
-    },
-  };
-
   if (process.env.ANALYZE === "true") {
     const withBundleAnalyzer = BundleAnalyzer({
       enabled: true,
