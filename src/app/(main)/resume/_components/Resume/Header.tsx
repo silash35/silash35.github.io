85a48c3a2baea1574e6381e0cfdfdab7ac62d94a
Silas Henrique <silash35@gmail.com>
2026-01-23 18:42:41 -0300

    Improve resume page design

--- src/app/(main)/resume/_components/Resume/Header.tsx
+++ src/app/(main)/resume/_components/Resume/Header.tsx
@@ -8,43 +8,42 @@ import profileImage from "@/images/profile.png";
 import styles from "./header.module.scss";
 
 const Header = () => (
-  <header className={styles.wrapper}>
-    <div className={styles.title}>
+  <header className={styles.header}>
+    <div className={styles.container}>
       <h1>Silas Henrique Alves Araújo</h1>
-
-      <ImageModal
-        imageProps={{
-          src: profileImage,
-          alt: "Photo of silas Henrique Alves Araújo",
-          loading: "lazy",
-          sizes: "18vw",
-        }}
-        className={styles.photo}
-      />
+      <ul className={styles.information}>
+        <li>
+          <CakeIcon />
+          February 2, 2003
+        </li>
+        <li>
+          <EmailIcon />
+          <a href="mailto:silash35@gmail.com" title="Silas Henrique’s e-mail address">
+            silash35@gmail.com
+          </a>
+        </li>
+        <li>
+          <PhoneIcon />
+          <a href="tel:+5571987529296" title="Phone number of Silas Henrique">
+            +55 71 98752-9296
+          </a>
+        </li>
+        <li>
+          <MarkerIcon />
+          Simões Filho, Bahia, Brazil
+        </li>
+      </ul>
     </div>
 
-    <ul className={styles.information}>
-      <li>
-        <CakeIcon />
-        February 2, 2003
-      </li>
-      <li>
-        <EmailIcon />
-        <a href="mailto:silash35@gmail.com" title="Silas Henrique’s e-mail address">
-          silash35@gmail.com
-        </a>
-      </li>
-      <li>
-        <PhoneIcon />
-        <a href="tel:+5571987529296" title="Phone number of Silas Henrique">
-          +55 71 98752-9296
-        </a>
-      </li>
-      <li>
-        <MarkerIcon />
-        Simões Filho, Bahia, Brazil
-      </li>
-    </ul>
+    <ImageModal
+      imageProps={{
+        src: profileImage,
+        alt: "Photo of silas Henrique Alves Araújo",
+        loading: "lazy",
+        sizes: "18vw",
+      }}
+      className={styles.photo}
+    />
   </header>
 );
 
